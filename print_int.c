5b3d150157cc08371ac91bc9a6122facaac7fa29
abdiye <bentaabdi@gmail.com>
2022-08-13 06:28:16 -0700

    print_int.c

---
+++ print_int.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ *prente_d - prints decimal
+ *@args: arguments
+ *Return: number of print
+ */
+int prente_d(va_list args)
+{
+int a[10];
+int j, m, n, sum, count;
+n = va_arg(args, int);
+count = 0;
+m = 1000000000;
+a[0] = n / m;
+for (j = 1; j < 10; j++)
+{
+m /= 10;
+a[j] = (n / m) % 10;
+}
+if (n < 0)
+{
+_putchar('-');
+count++;
+for (j = 0; j < 10; j++)
+a[j] *= -1;
+}
+for (j = 0, sum = 0; j < 10; j++)
+{
+sum += a[j];
+if (sum != 0 || j == 9)
+{
+_putchar('0' + a[j]);
+count++;
+}
+}
+return (count);
+}
